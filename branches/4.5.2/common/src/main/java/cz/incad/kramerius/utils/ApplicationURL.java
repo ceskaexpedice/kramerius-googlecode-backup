9a540663d0cda6f247d2046023bb13ddd0517051
vlahoda <vlahoda@fb5fbca2-9ed5-11de-81d6-937ba7f528c2>
2012-06-26 07:50:30 +0000

    Fixes Issue 352. Wrong port in editor base URL.

--- branches/4.5.2/common/src/main/java/cz/incad/kramerius/utils/ApplicationURL.java
+++ branches/4.5.2/common/src/main/java/cz/incad/kramerius/utils/ApplicationURL.java
@@ -10,63 +10,72 @@ import javax.servlet.http.HttpServletRequest;
 
 import cz.incad.kramerius.utils.conf.KConfiguration;
 
+/**
+ * Helper class for determining application URL. 
+ * @author pavels
+ */
 public class ApplicationURL {
 
-	public static final java.util.logging.Logger LOGGER = java.util.logging.Logger
-			.getLogger(ApplicationURL.class.getName());
+    public static final java.util.logging.Logger LOGGER = java.util.logging.Logger
+            .getLogger(ApplicationURL.class.getName());
+
 	
-	public static String getServerAndPort(HttpServletRequest request) {
+	/**
+	 * Returns protocol, server and port 
+	 * @param request Processing request
+	 * @return
+	 */
+    public static String getServerAndPort(HttpServletRequest request) {
         try {
             String string = request.getRequestURL().toString();
             URL url = new URL(string);
-            return url.getProtocol()+"://"+url.getHost()+":"+url.getPort();
+            return url.getProtocol()+"://"+url.getHost()+extractPort(url);
         } catch (MalformedURLException e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
             return "<no url>";
         }
-	    
-	}
 
-	
-	public static String applicationURL(HttpServletRequest request) {
-		//"dvju"
-		try {
-			String string = request.getRequestURL().toString();
-			URL url = new URL(string);
-			String application = applicationContextPath(url);
-			
-			String port = extractPort(url);
+    }
+
+	/**
+	 * Returns full application URL
+	 * @param request HTTP Request
+	 * @return full application URL
+	 */
+    public static String applicationURL(HttpServletRequest request) {
+        try {
+            String string = request.getRequestURL().toString();
+            URL url = new URL(string);
+            String application = applicationContextPath(url);
+
+            String port = extractPort(url);
             String aURL = url.getProtocol() + "://" + url.getHost() + port + "/" + application;
-			return aURL;
-		} catch (MalformedURLException e) {
-			LOGGER.log(Level.SEVERE, e.getMessage(), e);
-			return "<no url>";
-		}
-	}
+            return aURL;
+        } catch (MalformedURLException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            return "<no url>";
+        }
+    }
 
-
+	/**
+	 * Extracts port from given url or return empty string (if port is < 0)
+	 * @param url
+	 * @return
+	 */
     public static String extractPort(URL url) {
         if (url.getPort() > 0) {
             return ":"+url.getPort();
         } else return "";
     }
 
-	/*
-	   public static String applicationURL(HttpServletRequest request) {
-	        try {
-	            String string = request.getRequestURL().toString();
-	            URL url = new URL(string);
-	            url.getFile();
-	            String application = applicationContextPath(url);
-	            String aURL = url.getProtocol()+"://"+url.getHost()+":"+url.getPort()+"/"+application;
-	            return aURL;
-	        } catch (MalformedURLException e) {
-	            LOGGER.log(Level.SEVERE, e.getMessage(), e);
-	            return "<no url>";
-	        }
-	    }
-*/
-	
+
+    
+    /**
+     * Returns application context from given url
+     * @param url Processing url
+     * @return
+     */
+
     public static String applicationContextPath(URL url) {
         String path = url.getPath();
         String application = path;
@@ -74,6 +83,12 @@ public class ApplicationURL {
         if (tokenizer.hasMoreTokens()) application = tokenizer.nextToken();
         return application;
     }
+    
+    /**
+     * Returns application context from given reqest
+     * @param request Processing request
+     * @return
+     */
     public static String applicationContextPath(HttpServletRequest request) {
         try {
             String string = request.getRequestURL().toString();
@@ -85,30 +100,30 @@ public class ApplicationURL {
         }
     }
 
-	public static String urlOfPath(HttpServletRequest request,  String path) {
-		KConfiguration conf = KConfiguration.getInstance();
-		if ((conf.getApplicationURL() != null) && (!conf.getApplicationURL().equals(""))) {
-			return conf.getApplicationURL() +path;
-		} else {
-			return applicationURL(request)+"/"+path;
-		}
-	}
-	
-	public static void main(String[] args) throws MalformedURLException {
+    public static String urlOfPath(HttpServletRequest request,  String path) {
+        KConfiguration conf = KConfiguration.getInstance();
+        if ((conf.getApplicationURL() != null) && (!conf.getApplicationURL().equals(""))) {
+            return conf.getApplicationURL() +path;
+        } else {
+            return applicationURL(request)+"/"+path;
+        }
+    }
+
+    public static void main(String[] args) throws MalformedURLException {
         String surl = "http://localhost:8080/search/";
         URL url = new URL(surl);
         String file = url.getFile();
         String query = url.getQuery();
         System.out.println(file);
         System.out.println(query);
-        
+
         System.out.println(minus(surl,"?"+query));
-	}
-	
-	public static String minus(String bigger, String smaller) {
-	    if (bigger.length() > smaller.length()) {
+    }
+
+    public static String minus(String bigger, String smaller) {
+        if (bigger.length() > smaller.length()) {
             return bigger.replace(smaller, "");
-	    } else throw new IllegalArgumentException("");
-	}
+        } else throw new IllegalArgumentException("");
+    }
 
 }
