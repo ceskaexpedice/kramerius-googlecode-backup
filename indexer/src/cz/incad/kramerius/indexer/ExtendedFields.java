373c48c18bc45df0fb774f66fe93380c6dea772d
vlahoda@gmail.com <vlahoda@gmail.com@fb5fbca2-9ed5-11de-81d6-937ba7f528c2>
2014-01-09 15:27:11 +0000

    added loadNonSeq pdf parser (KNAV patch), marked version 4.8.3

--- indexer/src/cz/incad/kramerius/indexer/ExtendedFields.java
+++ indexer/src/cz/incad/kramerius/indexer/ExtendedFields.java
@@ -101,11 +101,17 @@ public class ExtendedFields {
                 pdfPid = "";
                 closePDFDocument();
                 InputStream is = fa.getDataStream(pid, "IMG_FULL");
-                PDFParser parser = new PDFParser(is);
-                parser.parse();
-                cosDoc = parser.getDocument();
+                if (KConfiguration.getInstance().getConfiguration().getBoolean("convert.pdf.loadNonSeq", false)){
+                    PDDocument pdDocument = PDDocument.loadNonSeq(is, null);
+                    cosDoc = pdDocument.getDocument();
+                    pdDoc = new PDDocument(cosDoc);
+                }else{
+                    PDFParser parser = new PDFParser(is);
+                    parser.parse();
+                    cosDoc = parser.getDocument();
+                    pdDoc = new PDDocument(cosDoc);
+                }
 
-                pdDoc = new PDDocument(cosDoc);
                 pdfPid = pid;
 
                 if( pdDoc.isEncrypted() ){
