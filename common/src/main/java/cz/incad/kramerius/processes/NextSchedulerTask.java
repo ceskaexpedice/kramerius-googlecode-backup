e9c344ce5858e371a1dc8483c00d2917e7f52f5a
vlahoda@gmail.com <vlahoda@gmail.com@fb5fbca2-9ed5-11de-81d6-937ba7f528c2>
2014-02-20 14:38:56 +0000

    Process manager debug logging

--- common/src/main/java/cz/incad/kramerius/processes/NextSchedulerTask.java
+++ common/src/main/java/cz/incad/kramerius/processes/NextSchedulerTask.java
@@ -1,10 +1,10 @@
 package cz.incad.kramerius.processes;
 
+import cz.incad.kramerius.utils.conf.KConfiguration;
+
 import java.util.List;
 import java.util.TimerTask;
 import java.util.logging.Level;
-
-import cz.incad.kramerius.utils.conf.KConfiguration;
 
 /**
  * Scheduler is able to start new process
@@ -30,8 +30,10 @@ public class NextSchedulerTask extends TimerTask {
 	public void run() {
 		try {
 			// neco delej
+            LOGGER.fine("Process scheduler run started.");
 			definitionManager.load();
 			List<LRProcess> plannedProcess = lrProcessManager.getPlannedProcess(allowRunningProcesses());
+			LOGGER.fine("Planned processes :"+plannedProcess.size());
 			if (!plannedProcess.isEmpty()) {
 				int runningProcesses = 0;
 				List<LRProcess> longRunningProcesses = this.lrProcessManager.getLongRunningProcessesAsFlat(null, null, null);
@@ -40,12 +42,13 @@ public class NextSchedulerTask extends TimerTask {
 						runningProcesses +=1;
 					}
 				}
+                LOGGER.fine("Running processes :" +runningProcesses);
 				if (runningProcesses < allowRunningProcesses()) {
 					LRProcess lrProcess = plannedProcess.get(0);
 					lrProcess.startMe(false, this.processScheduler.getApplicationLib(), this.processScheduler.getAdditionalJarFiles());
 				}
 			}  else {
-				LOGGER.fine("no planned process found");
+				LOGGER.fine("No planned process found");
 			}
 			this.processScheduler.scheduleNextTask();
 		} catch(Throwable e) {
